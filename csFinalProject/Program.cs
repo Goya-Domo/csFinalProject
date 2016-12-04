98b14bcda7e82ed2cd5e7ce29490e5b4850a2daa
Marshall Reisdorph <mkreisdorph@gmail.com>
2016-12-03 23:33:55 -0600

    default users added

--- csFinalProject/Program.cs
+++ csFinalProject/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace csFinalProject
 {
@@ -17,6 +18,39 @@ namespace csFinalProject
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            
+            SqlConnection connection = pchrDB.getConnection();
+            SqlCommand addCommand = new SqlCommand();
+            string command = "INSERT UserList "
+                + "(PATIENT_ID, UserName, PassHash) "
+                + "VALUES (@P_ID, @UserName, @PassHash)" 
+                + "INSERT UserList "
+                + "(PATIENT_ID, UserName, PassHash) "
+                + "VALUES (@P_ID2, @UserName2, @PassHash2)";
+
+            addCommand.Parameters.AddWithValue("@P_ID", "000001");
+            addCommand.Parameters.AddWithValue("@UserName", "smithjohn");
+            addCommand.Parameters.AddWithValue("@PassHash", pchrDB.getHashString("password"));
+            addCommand.Parameters.AddWithValue("@P_ID2", "000002");
+            addCommand.Parameters.AddWithValue("@UserName2", "doejane");
+            addCommand.Parameters.AddWithValue("@PassHash2", pchrDB.getHashString("password"));
+            addCommand.CommandText = command;
+            addCommand.Connection = connection;
+
+            connection.Open();
+            try
+            {
+                addCommand.ExecuteNonQuery().ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+
             frmLogin fLogin = new frmLogin();
             if (fLogin.ShowDialog() == DialogResult.OK)
             {
